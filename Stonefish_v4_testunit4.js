9731d10e96caed1b9db526bce9126d40a082a89c
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-11 22:58:47 +1200

    Remove superseded v4 testunit4

--- Stonefish_v4_testunit4.js
+++
@@ -1,9 +0,0 @@
-// Stonefish_v4(testunit4)
-// Same v3 core and same five v4 tie-breakers.
-// Experiment: king safety before checks.
-
-const STONEFISH_V4_TESTUNIT4_ORDER = ['kingFreedom', 'kingProtection', 'check', 'mobility', 'center'];
-
-function getStonefishV4TestUnit4Move(game) {
-  return getStonefishV4MoveWithOrder(game, STONEFISH_V4_TESTUNIT4_ORDER);
-}
