9879d5fefd47623b9fabe64bdb6186730e4d8f46
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-12 15:16:44 +1200

    Benchmark self-contained Stonefish v5

--- benchmark_v5.js
+++ benchmark_v5.js
@@ -13,8 +13,7 @@ const engineFiles = [
   'Stonefish_v4_5_opening_overrides.js',
   'Stonefish_v4_5_safety_patch.js',
   'Stonefish_v4_5_balance_patch.js',
-  'Stonefish_v5.js',
-  'Stonefish_v5_tuning_patch.js'
+  'Stonefish_v5.js'
 ];
 
 const source = engineFiles.map(file => fs.readFileSync(file, 'utf8')).join('\n\n');
