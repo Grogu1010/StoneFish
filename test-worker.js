2963181f0d85e9605b55e293ac412953d19730fb
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-12 14:55:51 +1200

    Remove v5 prototype from worker registry

--- test-worker.js
+++ test-worker.js
@@ -7,9 +7,9 @@ importScripts(
   './Stonefish_v4_5.js',
   './Stonefish_v4_5_opening_overrides.js',
   './Stonefish_v4_5_safety_patch.js',
-  './Stonefish_v5_testunit1.js',
   './Stonefish_v4_5_balance_patch.js',
-  './Stonefish_v5.js'
+  './Stonefish_v5.js',
+  './Stonefish_v5_tuning_patch.js'
 );
 
 const workerModels = {
@@ -18,7 +18,6 @@ const workerModels = {
   v3: getStonefishV3Move,
   v4: getStonefishV4Move,
   v45: getStonefishV45Move,
-  v5t1: getStonefishV5Testunit1Move,
   v5: getStonefishV5Move
 };
 
