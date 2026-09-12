36990ea725a8a40d81c3f71a6b4ad1401cddbcff
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-12 21:56:54 +1200

    Benchmark bookless v5

--- benchmark_v5.js
+++ benchmark_v5.js
@@ -13,7 +13,8 @@ const engineFiles = [
   'Stonefish_v4_5_opening_overrides.js',
   'Stonefish_v4_5_safety_patch.js',
   'Stonefish_v4_5_balance_patch.js',
-  'Stonefish_v5.js'
+  'Stonefish_v5.js',
+  'Stonefish_v5_no_opening_book_patch.js'
 ];
 
 const source = engineFiles.map(file => fs.readFileSync(file, 'utf8')).join('\n\n');
