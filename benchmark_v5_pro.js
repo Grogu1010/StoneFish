5d64f01c29678ea131f2a70128f717a30595eb09
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-13 14:43:19 +1200

    Load one-pass move patch in Pro benchmark

--- benchmark_v5_pro.js
+++ benchmark_v5_pro.js
@@ -23,6 +23,9 @@ if (fs.existsSync('Stonefish_v5_pro_geometry_patch.js')) {
 if (fs.existsSync('Stonefish_runtime_speed_patch.js')) {
   engineFiles.push('Stonefish_runtime_speed_patch.js');
 }
+if (fs.existsSync('Stonefish_fast_moves_experiment.js')) {
+  engineFiles.push('Stonefish_fast_moves_experiment.js');
+}
 
 const source = engineFiles.map(file => fs.readFileSync(file, 'utf8')).join('\n\n');
 vm.runInThisContext(source, { filename: 'stonefish-v5-pro-benchmark-bundle.js' });
