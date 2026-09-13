6d4db0380b2535446374ea4be403566e780b594b
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-13 15:30:06 +1200

    Load v5 Pro before ARMX in dev worker

--- test-worker.js
+++ test-worker.js
@@ -9,13 +9,13 @@ importScripts(
   './Stonefish_v4_5_safety_patch.js',
   './Stonefish_v4_5_balance_patch.js',
   './Stonefish_v5.js',
-  './ARMX_preview_fast.js',
-  './Stonefish_v5_5_testunit1.js',
   './Stonefish_v5_pro.js',
   './Stonefish_v5_pro_speed_patch.js',
   './Stonefish_v5_pro_geometry_patch.js',
   './Stonefish_runtime_speed_patch.js',
-  './Stonefish_fast_moves_experiment.js'
+  './Stonefish_fast_moves_experiment.js',
+  './ARMX_preview_fast.js',
+  './Stonefish_v5_5_testunit1.js'
 );
 
 const workerModels = {
