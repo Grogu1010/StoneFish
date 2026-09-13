fc5b6a89c042a43e35aad4fea4345a26fb85c52f
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-13 17:54:27 +1200

    Restore tighter v5.5 semifinal gate for speed

--- Stonefish_v5_5_search.js
+++ Stonefish_v5_5_search.js
@@ -9,7 +9,7 @@
 
 const STONEFISH_V5_5_SEARCH = Object.freeze({
   name: 'ARMX-guided Guarded PVS',
-  semifinalists: 8,
+  semifinalists: 5,
   rootCandidates: 4,
   branch: [0, 1, 2, 2, 4],
   lmrMinDepth: 3,
