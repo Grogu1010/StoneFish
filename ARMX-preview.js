af6210e90d2e572d1d6a4672fcc4de3d7420f71c
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-22 09:07:30 +1200

    Probe 60k-node compiled ARMX strength

--- ARMX-preview.js
+++ ARMX-preview.js
@@ -52,7 +52,7 @@ const ARMX_PREVIEW = Object.freeze({
   evidenceSearchNodes: 400,
   fullSearchEvidence: 8,
   maxExtraSearchDepth: 1,
-  nativeSearchNodes: 30000,
+  nativeSearchNodes: 60000,
   nativeSearchDepth: 6,
 });
 
