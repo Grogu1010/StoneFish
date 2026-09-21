1b24662602227178b6efc4773b5306175bb25e7a
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-22 11:10:19 +1200

    Order compiled ARMX moves lazily

--- native/v5_5_helpers.c
+++ native/v5_5_helpers.c
@@ -706,13 +706,21 @@ static int search_order(u32 m,int ply,int tt_move){
   if(search_policy_enabled&&(ply&1))value+=js_round(300.0*policy_logit(m));
   return value;
 }
-static void search_sort(u32 *moves,int n,int ply,int tt_move){
-  int priorities[512];
-  for(int i=0;i<n;i++){
-    u32 m=moves[i];int p=search_order(m,ply,tt_move),j=i-1;
-    while(j>=0&&priorities[j]<p){moves[j+1]=moves[j];priorities[j+1]=priorities[j];j--;}
-    moves[j+1]=m;priorities[j+1]=p;
+static int search_order_priorities[32][512];
+static int *search_prepare_order(u32 *moves,int n,int ply,int tt_move){
+  int *priorities=search_order_priorities[ply<32?ply:31];
+  for(int i=0;i<n;i++)priorities[i]=search_order(moves[i],ply,tt_move);
+  return priorities;
+}
+static u32 search_pick_ordered(u32 *moves,int *priorities,int n,int index){
+  int best=index;
+  for(int i=index+1;i<n;i++)if(priorities[i]>priorities[best])best=i;
+  if(best!=index){
+    u32 move=moves[best];int priority=priorities[best];
+    for(int i=best;i>index;i--){moves[i]=moves[i-1];priorities[i]=priorities[i-1];}
+    moves[index]=move;priorities[index]=priority;
   }
+  return moves[index];
 }
 
 static int search_q(SearchState *s,int alpha,int beta,int ply,int remaining){
@@ -739,10 +747,10 @@ static int search_q(SearchState *s,int alpha,int beta,int ply,int remaining){
     if(!n)return search_generate(s,2)?stand:0;
     for(int i=0;i<n;i++)moves[i]=output[i];
   }
-  search_sort(moves,n,ply,0);
+  int *priorities=search_prepare_order(moves,n,ply,0);
   if(pos)search_enter_position(pos);
   for(int i=0;i<n;i++){
-    u32 m=moves[i];
+    u32 m=search_pick_ordered(moves,priorities,n,i);
     if(!check&&!move_promotion(m)&&stand+config[move_captured(m)]+160<alpha)continue;
     SearchUndo u;search_apply(s,m,&u);
     int score=-search_q(s,-beta,-alpha,ply+1,remaining-1);
@@ -779,11 +787,11 @@ static int search_ab(SearchState *s,int depth,int alpha,int beta,int ply,u32 las
   if(search_draw(s,pos))return 0;
   if(!budget_live){search_abort=1;return search_evaluate_state(s);}
   u32 moves[512];for(int i=0;i<n;i++)moves[i]=output[i];
-  search_sort(moves,n,ply,hit?hit->move:0);
+  int *priorities=search_prepare_order(moves,n,ply,hit?hit->move:0);
   int best=-SEARCH_MATE,best_move=0,index=0;
   search_enter_position(pos);
   for(int i=0;i<n;i++){
-    u32 m=moves[i];SearchUndo u;search_apply(s,m,&u);
+    u32 m=search_pick_ordered(moves,priorities,n,i);SearchUndo u;search_apply(s,m,&u);
     int quiet=!move_captured(m)&&!move_promotion(m),score;
     if(index==0)score=-search_ab(s,depth-1,-beta,-alpha,ply+1,m);
     else{
