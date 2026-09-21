f2658fdf4cd5302d9ccc54463bf10ea0871cba55
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-22 09:09:13 +1200

    Restore 30k compiled budget for TT test

--- ARMX-preview.js
+++ ARMX-preview.js
@@ -52,7 +52,7 @@ const ARMX_PREVIEW = Object.freeze({
   evidenceSearchNodes: 400,
   fullSearchEvidence: 8,
   maxExtraSearchDepth: 1,
-  nativeSearchNodes: 60000,
+  nativeSearchNodes: 30000,
   nativeSearchDepth: 6,
 });
 
