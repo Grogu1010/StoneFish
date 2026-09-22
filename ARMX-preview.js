8c37cc388a983d5677ae9d42215f3bfa7812fe51
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-22 16:28:38 +1200

    Test 80% ARMX extra search effort

--- ARMX-preview.js
+++ ARMX-preview.js
@@ -46,8 +46,8 @@ const ARMX_PREVIEW = Object.freeze({
   quietChoiceWeightLimit: 6,
   predictionQualityDecay: 0.9,
   predictionSurpriseScale: 0.3,
-  maxExtraSearchNodes: 3600,
-  evidenceSearchNodes: 4800,
+  maxExtraSearchNodes: 2880,
+  evidenceSearchNodes: 3840,
   fullSearchEvidence: 8,
   maxExtraSearchDepth: 2,
 });
