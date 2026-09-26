ba8900b89672a2d96c95258180412cf98ab05cae
Grogu1010 <107981543+Grogu1010@users.noreply.github.com>
2026-09-26 18:30:48 +1200

    Ablate Full ARMX earned search effort

--- ARMX/ARMX.js
+++ ARMX/ARMX.js
@@ -48,9 +48,9 @@ const ARMX_FULL = Object.freeze({
   // v5.5 host budget/width and earns extra analysis only from opponent evidence.
   candidateLimit: 4,
   baseSearchNodes: 1200,
-  maxEvidenceSearchNodes: 2400,
-  maxSurpriseSearchNodes: 1200,
-  maxExtraNodes: 3600,
+  maxEvidenceSearchNodes: 0,
+  maxSurpriseSearchNodes: 0,
+  maxExtraNodes: 0,
   baseDepth: 4,
   maxEvidenceDepth: 6,
   maxExtraDepth: 2,
